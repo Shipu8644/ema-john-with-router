feb0f6da85e8546e3b4a03021e7ad04022e31a13
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-02 13:40:15 +0600

    order review done

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 
 import ReviewItem from '../ReviewItem/ReviewItem';
@@ -9,11 +10,18 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 const OrderReview = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+    const history = useHistory();
     const handleRemove = (key) => {
 
         const newCart = cart.filter(product => product.key !== key);
         setCart(newCart);
         removeFromDb(key);
+    }
+
+    const handlePlaceOrder = () => {
+        history.push('/placeorder');
+        setCart([]);
+        clearTheCart();
     }
     return (
         <div>
@@ -30,7 +38,9 @@ const OrderReview = () => {
                     }
                 </div>
                 <div className="cart-container">
-                    <Cart cart={cart}></Cart>
+                    <Cart cart={cart}>
+                        <button onClick={handlePlaceOrder} className='btn-regular'>Place Order</button>
+                    </Cart>
                 </div>
             </div>
         </div>
